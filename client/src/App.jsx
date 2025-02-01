e11b5196ff3d81e5f59d1a5e5bca2307b08d1590
unknown <natnaelm552@gmail.com>
2025-02-01 10:13:03 +0300

    shopping view setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,18 @@ import Dashboard from "./pages/admin-view/Dashboard"
 import Features from "./pages/admin-view/Features"
 import Orders from "./pages/admin-view/Orders"
 import Products from "./pages/admin-view/Products"
+import ShoppingLayout from "./components/shopping-view/layout"
+import NotFound from "./pages/NotFound"
+import ShoppingHome from "./pages/shopping-view/Home"
+import ShoppingListing from "./pages/shopping-view/Listing"
+import ShoppingCheckout from "./pages/shopping-view/Checkout"
+import ShoppingAccount from "./pages/shopping-view/Account"
 
 function App() {
 
 
   return (
     <div className="flex flex-col overflow-hidden bg-background w-full h-screen" >
-      <h1>Welcome To Gebeya</h1>
       <Routes >
         <Route path="/auth" element={<AuthLayout/>}>
           <Route path="login" element={<Login/>}/>
@@ -25,8 +30,18 @@ function App() {
          <Route path = 'features' element = {<Features/>}/>
          <Route path = 'orders' element = {<Orders/>}/>
          <Route path = 'products' element = {<Products/> }/>
+        </Route>
+
+        <Route path="/shop" element={<ShoppingLayout/>}>
+          <Route path="home" element= {<ShoppingHome/>}/> 
+          <Route path="listing" element= {<ShoppingListing/>}/>
+          <Route path="checkout" elemnt = {<ShoppingCheckout/>}/>
+          <Route path="account" elemnt = {<ShoppingAccount/>}/>
+
 
         </Route>
+
+        <Route path='*' element={<NotFound/>}/>
 
       </Routes>
 

eb576f49f7ceac791685dcf06f8152a4047dd264
unknown <natnaelm552@gmail.com>
2025-03-12 10:39:46 +0300

    filter and sort on frontend

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -1,40 +1,49 @@
-import { Fragment } from "react"
-import {filterOptions} from "@/config"
-import { Checkbox } from "../ui/checkbox"
-import { Separator } from "../ui/separator"
+import { Fragment } from "react";
+import { filterOptions } from "@/config";
+import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
 
-const ProductFilter = () => {
+const ProductFilter = ({ filters, handleFilter }) => {
   return (
     <div>
-        <div className="p-4 border-b mb-4">
-            <h2 className="text-2xl font-bold">Filters</h2>
-        </div>
-        <div>
-            {
-                Object.keys(filterOptions).map((key) => {
-                    return(
-                    <Fragment>
-                        <div>
-                            <h3 className="text-lg font-semibold mt-6">{key}</h3>
-                        </div>
-                        <div>
-                            {filterOptions[key].map((option) => {
-                                return(
-                                    <div key={option.id} className="flex items-center gap-2 py-[2px] my-1">
-                                        <Checkbox/>
-                                        <label>{option.label}</label>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <Separator />
-                    </Fragment>
-                    )
-                })
-            }
-        </div>
+      <div className="p-4 border-b mb-4">
+        <h2 className="text-2xl font-bold">Filters</h2>
+      </div>
+      <div>
+        {Object.keys(filterOptions).map((key) => {
+          return (
+            <Fragment>
+              <div>
+                <h3 className="text-lg font-semibold mt-6">{key}</h3>
+              </div>
+              <div>
+                {filterOptions[key].map((option) => {
+                  return (
+                    <div
+                      key={option.id}
+                      className="flex items-center gap-2 py-[2px] my-1"
+                    >
+                      <Checkbox
+                        onCheckedChange={() => handleFilter(key, option.id)}
+                        checked={
+                          filters &&
+                          Object.keys(filters).length > 0 &&
+                          filters[key] &&
+                          filters[key].indexOf(option.id) > -1
+                        }
+                      />
+                      <label>{option.label}</label>
+                    </div>
+                  );
+                })}
+              </div>
+              <Separator />
+            </Fragment>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductFilter
+export default ProductFilter;
--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -4,6 +4,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
+  DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -12,26 +13,79 @@ import { ArrowUpDownIcon } from "lucide-react";
 import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
 import { useDispatch, useSelector } from "react-redux";
 import ShoppingProductTile from "@/components/shopping-view/product-tile";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { createSearchParams, useSearchParams } from "react-router-dom";
+
+function createSearchParamsHelper(filters) {
+  const queryParams = [];
+  for (const [key, value] of Object.entries(filters)) {
+    if (Array.isArray(value) && value.length > 0) {
+      const paramValue = value.join(",");
+      queryParams.push(`${key}=${encodeURIComponent(paramValue)}`);
+    }
+  }
+
+  return queryParams.join("&");
+}
 
 const ShoppingListing = () => {
   const dispatch = useDispatch();
   const { productList } = useSelector((state) => state.shopProducts);
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState(null);
+  const [searchParmas, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setFilters(JSON.parse(sessionStorage.getItem("filters")) || {});
+  }, []);
 
   useEffect(() => {
     dispatch(fetchAllFilteredProducts());
   }, []);
 
-  console.log("productList", productList);
+  useEffect(() => {
+    if (filters && Object.keys(filters).length > 0) {
+      const createQueryString = createSearchParamsHelper(filters);
+      setSearchParams(new URLSearchParams(createQueryString));
+    }
+  }, [filters]);
+  function handleSort(value) {
+    setSort(value);
+  }
+
+  function handleFilter(getSectionId, getCurrentOption) {
+    console.log(getSectionId, getCurrentOption);
+    let cpyFilters = { ...filters };
+    const indexOfCurrentSection = Object.keys(cpyFilters).indexOf(getSectionId);
+    if (indexOfCurrentSection === -1) {
+      cpyFilters = {
+        ...cpyFilters,
+        [getSectionId]: [getCurrentOption],
+      };
+    } else {
+      const indexOfCurrentOption =
+        cpyFilters[getSectionId].indexOf(getCurrentOption);
+      if (indexOfCurrentOption === -1)
+        cpyFilters[getSectionId].push(getCurrentOption);
+      else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1);
+    }
+
+    setFilters(cpyFilters);
+    sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+  }
+
+  console.log(filters);
 
   return (
-    <div className="grid cols-1 md:grid-cols-[300px_1fr] p-4 gap-6">
-      <ProductFilter />
+    <div className="grid cols-1 md:grid-cols-[200px_1fr] p-4 gap-6">
+      <ProductFilter filters={filters} handleFilter={handleFilter} />
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex justify-between items-center">
           <h2 className="text-lg font-extrabold">All Products</h2>
           <div className=" flex items-center gap-2">
-            <span className="text-md text-muted-foreground">10 Products</span>
+            <span className="text-md text-muted-foreground">
+              {productList.length} Products
+            </span>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="flex items-center gap-1">
@@ -40,17 +94,17 @@ const ShoppingListing = () => {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuGroup>
-                  {sortOptions.map((option) => (
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
+                  {sortOptions.map((sortItem) => (
                     <DropdownMenuRadioItem
-                      key={option.id}
-                      value={option.id}
+                      key={sortItem.id}
+                      value={sortItem.id}
                       className="capitalize"
                     >
-                      {option.label}
+                      {sortItem.label}
                     </DropdownMenuRadioItem>
                   ))}
-                </DropdownMenuGroup>
+                </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
